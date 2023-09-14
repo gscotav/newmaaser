51ea6d434c6229cf4d7b026a6e03ae14097e9313
gscotav <gscotav@gmail.com>
2023-09-14 16:38:03 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-database.js";
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 // Import the functions you need from the SDKs you need
@@ -31,7 +30,7 @@ set(dataRef, {
 });
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   // Function to write data to the database using a separate button
   const writeToDatabaseSeparateButton = () => {
@@ -49,26 +48,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
       <div>
         <button onClick={writeToDatabaseSeparateButton}>
           Write Data to Firebase (Separate Button)
